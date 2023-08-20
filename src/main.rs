69808f3a3e446fb49c32b05eafd9d9d005408e49
Shota Kuchiki <zk5zg3az@i.softbank.jp>
2023-08-21 08:52:31 +0900

    Finish lesson

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-mod ownership;
-mod stack_heap;
-mod vars;
+// mod ownership;
+// mod stack_heap;
+// mod vars;
+// mod generics;
+// mod lifetime;
+// mod structs;
+// mod enums;
+// mod traits;
+extern crate lib_demo;
+mod debug;
+mod error_handling;
+mod unit_test;
 
 fn main() {
     // println!("Hello, world!");
@@ -8,5 +17,13 @@ fn main() {
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
     // stack_heap::run()
-    ownership::run();
+    // ownership::run();
+    // generics::run();
+    // lifetime::run();
+    // structs::run();
+    // enums::run();
+    // traits::run();
+    error_handling::run();
+    lib_demo::print_random_number();
+    debug::run();
 }
